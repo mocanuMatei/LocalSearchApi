d95b1d39ce9447b6f1e68a74483a9023f6163731
mateiMocanu <malasoft.dev@gmail.com>
2022-06-30 17:02:55 +0300

    Initialized Spring project

---
+++ src/main/java/com/localsearch/localsearchapi/LocalSearchApiApplication.java
@@ -0,0 +1,13 @@
+package com.localsearch.localsearchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalSearchApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalSearchApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/localsearch/localsearchapi/LocalSearchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.localsearch.localsearchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalSearchApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
